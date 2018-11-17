ee7508f96f0fbb94745e7d0e13ac573b0f4bb83c
Kurashimaryou <kurashimaryou@gmail.com>
2018-11-17 15:52:46 +0900

    ver1

---
+++ myblogapp/posts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import post
+
+def index (request):
+ #   return HttpResponse("HelloWorld")
+     posts = post.objects.order_by('-published')
+     return render(request, 'posts/index.html', {'posts': posts})
+
+def post_detail(request,post_id):
+    post1 = get_object_or_404(post, pk=post_id)
+    return render(request, 'posts/post_detail.html', {'post2': post1})
+
+# Create your views here.
