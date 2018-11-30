d9c9443581d59615c1cf40468c1a13b445729ef8
KotaroWatanabe <rooonkota.12.1209@gmail.com>
2018-12-01 06:40:15 +0900

    <12/01 watanabe>urls.pyとviews.pyにページ遷移処理を実装した。

--- myblogapp/posts/urls.py
+++ myblogapp/posts/urls.py
@@ -3,4 +3,21 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = [url(r'^$',views.index, name='index')]
+urlpatterns = [
+        url(r'^$',views.index, name='index'),
+        url('yourprof',views.yourprof, name='yourprof'),
+        url('jfinal',views.jfinal, name='jfinal'),
+        url('loginpage',views.loginpage, name='loginpage'),
+        url('mypage',views.mypage, name='mypage'),
+        url('makepj',views.makepj, name='makepj'),
+        url('jlist',views.jlist, name='jlist'),
+        url('wantlist',views.wantlist, name='wantlist'),
+        url('makeprof',views.makeprof, name='makeprof'),
+        url('jhlist',views.jhlist, name='jhlist'),
+        url('jreal',views.jreal, name='jreal'),
+        url('mdlreview',views.mdlreview, name='mdlreview'),
+        url('finreview',views.finreview, name='finreview'),
+        url('bothreview',views.bothreview, name='bothreview'),
+        url('hreal',views.hreal, name='hreal'),
+        
+]
--- myblogapp/posts/views.py
+++ myblogapp/posts/views.py
@@ -11,4 +11,86 @@ def post_detail(request,post_id):
     post1 = get_object_or_404(post, pk=post_id)
     return render(request, 'posts/post_detail.html', {'post2': post1})
 
-# Create your views here.
+def yourprof(request):
+    param = {
+            'msg' :'相手のプロフィール画面',
+            }
+    return render(request, 'posts/yourprof.html',param)
+
+def jfinal(request):
+    param = {
+            'msg' :'応募最終確認画面',
+            }
+    return render(request, 'posts/jfinal.html',param)
+
+def loginpage(request):
+    param = {
+            'msg' :'ログイン画面',
+            }
+    return render(request, 'posts/loginpage.html',param)
+
+def mypage(request):
+    param = {
+            'msg' :'マイページ画面',
+            }
+    return render(request, 'posts/mypage.html',param)
+
+def makepj(request):
+    param = {
+            'msg' :'案件作成画面',
+            }
+    return render(request, 'posts/makepj.html',param)
+
+def jlist(request):
+    param = {
+            'msg' :'受注者候補一覧画面',
+            }
+    return render(request, 'posts/jlist.html',param)
+
+def wantlist(request):
+    param = {
+            'msg' :'やりたい案件一覧画面',
+            }
+    return render(request, 'posts/wantlist.html',param)
+
+def makeprof(request):
+    param = {
+            'msg' :'プロフィール作成画面',
+            }
+    return render(request, 'posts/makeprof.html',param)
+
+def jhlist(request):
+    param = {
+            'msg' :'受注/発注一覧画面',
+            }
+    return render(request, 'posts/jhlist.html',param)
+
+def jreal(request):
+    param = {
+            'msg' :'取引状況画面(受注)',
+            }
+    return render(request, 'posts/jreal.html',param)
+
+def mdlreview(request):
+    param = {
+            'msg' :'中間成果物レビュー画面',
+            }
+    return render(request, 'posts/mdlreview.html',param)
+
+def finreview(request):
+    param = {
+            'msg' :'最終成果物レビュー画面',
+            }
+    return render(request, 'posts/finreview.html',param)
+
+def bothreview(request):
+    param = {
+            'msg' :'双方レビュー画面',
+            }
+    return render(request, 'posts/bothreview.html',param)
+
+def hreal(request):
+    param = {
+            'msg' :'取引状況画面(発注)',
+            }
+    return render(request, 'posts/hreal.html',param)
